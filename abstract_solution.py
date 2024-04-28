80239555c4d0960ace58c48f15023c1c062bdae0
Juliusz Wasieleski <tojabonibykto@gmail.com>
2024-04-28 16:39:43 +0200

    adding cost function to abstract class

--- abstract_solution.py
+++ abstract_solution.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 import json
-from Data import Data
-from Result import Result
+from data import Data
+from result import Result
 
 
 class AbstractSolution(ABC):
@@ -13,6 +13,20 @@ class AbstractSolution(ABC):
         with open(self.path_to_solve, "r") as file:
             return Data(json.load(file))
 
+    def find_friend_interval_priority(self, friend, interval) -> int:
+        pass
+    def cost_function(self, result: Result, data:Data):
+        cost = 0
+
+        # Priorytety koleżków
+        for friend in result.friends:
+            cost -= self.find_friend_interval_priority(friend, (result.start, result.end))
+
+        # Ceny wakacji
+        for day in range(result.start, result.end + 1):
+            cost += data.prices[day] * data.alpha
+
+        return cost
     @abstractmethod
     def solve(self) -> Result:
         pass
--- genetic_julek.py
+++ genetic_julek.py
@@ -1,6 +1,6 @@
 from abstract_solution import AbstractSolution
-from Data import Data
-from Result import Result
+from data import Data
+from result import Result
 
 
 class GeneticJulek(AbstractSolution):
@@ -10,4 +10,6 @@ class GeneticJulek(AbstractSolution):
     def solve(self) -> Result:
         data = super().read_data_from_json()
         res = Result(data.D1, data.D2, [])
+        cost = super().cost_function(res, data)
+        print(cost)
         return res
